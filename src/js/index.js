b0679c5b5a0b00be494520972468a0d5a039ed6e
Navis Rina <karya.09@mail.ru>
2022-05-09 19:50:57 +0300

    popytka sdelat' kompilyatsiyu pug  trying to compile pug

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import './../scss/style.scss'
 (function (requireContext) {
     return requireContext.keys().map(requireContext);
 } (require.context('../', true, /(\.scss$)|(\.css$)/)));
-
 // function requireAll (r) { r.keys().forEach(r); }
 // requireAll(require.context('./', true, /\.pug$/));
 // var template = require('./../pug/includes/elements/buttons/buttons.pug');
