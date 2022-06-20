a8c349902bcaf0c667c0d66dec3f111637dfb031
Navis Rina <karya.09@mail.ru>
2022-06-20 23:53:35 +0300

    make js in like-button

--- src/pug/includes/elements/like button/like-button.js
+++ src/pug/includes/elements/like button/like-button.js
@@ -1,7 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function() {
-  var likeButton = document.getElementById('like-button');
+
+document.addEventListener('DOMContentLoaded', function() {
+  // let likeButton = document.getElementById('like-button');
+  let likeNumber = document.getElementByClass('like-number');
   likeButton.addEventListener('click', function() {
     // window.lb = likeButton;
-    likeButton.classList.toggle('selected');
+    likeButton.classList.add('selected');
+    // likeNumber.value + 1;
+    let like = parseInt(document.getElementByClass("like-button").text()) + 1;
+    // alert('lll');
+    // console.log('selected');
   });
-// }, false);
+}, false);
