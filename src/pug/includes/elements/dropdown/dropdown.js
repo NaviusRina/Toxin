92346e2a384fd22aeeeef6f10de4f79ea32d4a24
Navis Rina <karya.09@mail.ru>
2022-06-26 23:56:46 +0300

    do the dropdown

--- src/pug/includes/elements/dropdown/dropdown.js
+++ src/pug/includes/elements/dropdown/dropdown.js
@@ -1,9 +1,85 @@
-let guestNumberAdd = document.querySelectorAll('.guest-number__add');
-let guestNumberAmount = document.querySelector('.guest-number__amount');
 
-guestNumberAdd.forEach(function (entry) {
-  entry.addEventListener('click', function(event) {
-    event.preventDefault();
-    guestNumberAmount.textContent = parseInt(guestNumberAmount.textContent) + 1;
+
+// сделать как-то так, когда соображу как
+// let guestNumberAdd = document.querySelectorAll('.guest-number__add');
+// let guestNumberAmount = document.querySelector('.guest-number__amount');
+// guestNumberAdd.forEach(function (entry) {
+//   entry.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     guestNumberAmount.textContent = parseInt(guestNumberAmount.textContent) + 1;
+//   });
+// });
+
+//как стыдно-то... я исправлюсь
+
+let guestNumberAmount = document.querySelectorAll('.guest-number__amount');
+
+
+let guestNumberAdd1 = document.querySelector('.qna1');
+let guestNumberAmount1 = document.querySelector('.qnam1');
+
+guestNumberAdd1.addEventListener('click', function() {
+    guestNumberAmount1.textContent = parseInt(guestNumberAmount1.textContent) + 1;
   });
+
+let guestNumberDelete1 = document.querySelector('.qnd1');
+
+guestNumberDelete1.addEventListener('click', function() {
+  if(guestNumberAmount1.textContent == 0) {
+    guestNumberAmount1.textContent = 0;
+  }
+  else {
+    guestNumberAmount1.textContent = parseInt(guestNumberAmount1.textContent) - 1;
+  }
+  });
+
+
+let guestNumberAdd2 = document.querySelector('.qna2');
+let guestNumberAmount2 = document.querySelector('.qnam2');
+
+guestNumberAdd2.addEventListener('click', function() {
+    guestNumberAmount2.textContent = parseInt(guestNumberAmount2.textContent) + 1;
+  });
+
+let guestNumberDelete2 = document.querySelector('.qnd2');
+guestNumberDelete2.addEventListener('click', function() {
+    if(guestNumberAmount2.textContent == 0) {
+      guestNumberAmount2.textContent = 0;
+    }
+    else {
+      guestNumberAmount2.textContent = guestNumberAmount2.textContent - 1;
+    }
 });
+
+
+let guestNumberAdd3 = document.querySelector('.qna3');
+let guestNumberAmount3 = document.querySelector('.qnam3');
+
+guestNumberAdd3.addEventListener('click', function() {
+    guestNumberAmount3.textContent = parseInt(guestNumberAmount3.textContent) + 1;
+});
+
+let guestNumberDelete3 = document.querySelector('.qnd3');
+
+guestNumberDelete3.addEventListener('click', function() {
+  if(guestNumberAmount3.textContent == 0) {
+    guestNumberAmount3.textContent = 0;
+  }
+  else {
+    guestNumberAmount3.textContent = guestNumberAmount3.textContent - 1;
+  }
+});
+
+
+
+let dropdownBoxClear = document.querySelector('.dropdown-box__clear');
+// dropdownBoxButtonClear.forEach(function (entry) {
+  dropdownBoxClear.addEventListener('click', function() {
+    // event.preventDefault();
+    guestNumberAmount1.textContent = 0;
+    guestNumberAmount.forEach(function() {
+      guestNumberAmount.textContent = '0';
+    });
+
+  });
+// });
