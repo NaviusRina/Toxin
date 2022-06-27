251a9f4cce829bc714d9066db2c04eaceb0f23a7
Navis Rina <karya.09@mail.ru>
2022-06-27 23:09:34 +0300

    dropdown and dropdown-roon
    finished the main layout of dropdown and dropdown-roon

---
+++ src/pug/includes/elements/dropdown-room/dropdown-room.js
@@ -0,0 +1,91 @@
+// let guestNumberAmount = document.querySelectorAll('.guest-number__amount');
+
+// let dropdownRoomClear = document.querySelector('.dropdown-room__clear');
+
+
+let roomNumberAdd1 = document.querySelector('.rna1');
+let roomNumberAmount1 = document.querySelector('.rnam1');
+let roomNumberDelete1 = document.querySelector('.rnd1');
+
+roomNumberAdd1.addEventListener('click', function() {
+    roomNumberAmount1.textContent = parseInt(roomNumberAmount1.textContent) + 1;
+    // dropdownRoomClear.style.opacity = 1;
+    roomNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
+  });
+
+
+roomNumberDelete1.addEventListener('click', function() {
+  if(roomNumberAmount1.textContent == 0) {
+    roomNumberAmount1.textContent = 0;
+    roomNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+  }
+  else {
+    roomNumberAmount1.textContent = parseInt(roomNumberAmount1.textContent) - 1;
+  }
+  });
+
+
+let roomNumberAdd2 = document.querySelector('.rna2');
+let roomNumberAmount2 = document.querySelector('.rnam2');
+let roomNumberDelete2 = document.querySelector('.rnd2');
+
+roomNumberAdd2.addEventListener('click', function() {
+    roomNumberAmount2.textContent = parseInt(roomNumberAmount2.textContent) + 1;
+    // dropdownRoomClear.style.opacity = 1;
+    roomNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
+  });
+
+roomNumberDelete2.addEventListener('click', function() {
+    if(roomNumberAmount2.textContent == 0) {
+      roomNumberAmount2.textContent = 0;
+      roomNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+    }
+    else {
+      roomNumberAmount2.textContent = roomNumberAmount2.textContent - 1;
+    }
+});
+
+
+let roomNumberAdd3 = document.querySelector('.rna3');
+let roomNumberAmount3 = document.querySelector('.rnam3');
+let roomNumberDelete3 = document.querySelector('.rnd3');
+
+roomNumberAdd3.addEventListener('click', function() {
+    roomNumberAmount3.textContent = parseInt(roomNumberAmount3.textContent) + 1;
+    // dropdownRoomClear.style.opacity = 1;
+    roomNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
+});
+
+
+roomNumberDelete3.addEventListener('click', function() {
+  if(roomNumberAmount3.textContent == 0) {
+    roomNumberAmount3.textContent = 0;
+    roomNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+  }
+  else {
+    roomNumberAmount3.textContent = roomNumberAmount3.textContent - 1;
+  }
+});
+
+// guestNumberAmount.forEach(function(item) {
+//   if(item.textContent == 0) {
+//     dropdownBoxClear.style.opacity = 0;
+//   }
+// });
+
+
+
+// dropdownBoxButtonClear.forEach(function (entry) {
+  // dropdownRoomClear.addEventListener('click', function() {
+  //   // event.preventDefault();
+  //   roomNumberAmount1.textContent = 0;
+  //   roomNumberAmount2.textContent = 0;
+  //   roomNumberAmount3.textContent = 0;
+  //   dropdownRoomClear.style.opacity = 0;
+  //   roomNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+  //   roomNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+  //   roomNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+  //   // guestNumberAmount.textContent = 0;
+  //
+  // });
+// });
--- src/pug/includes/elements/dropdown/dropdown.js
+++ src/pug/includes/elements/dropdown/dropdown.js
@@ -14,19 +14,24 @@
 
 let guestNumberAmount = document.querySelectorAll('.guest-number__amount');
 
+let dropdownBoxClear = document.querySelector('.dropdown-box__clear');
+
 
 let guestNumberAdd1 = document.querySelector('.qna1');
 let guestNumberAmount1 = document.querySelector('.qnam1');
+let guestNumberDelete1 = document.querySelector('.qnd1');
 
 guestNumberAdd1.addEventListener('click', function() {
     guestNumberAmount1.textContent = parseInt(guestNumberAmount1.textContent) + 1;
+    dropdownBoxClear.style.opacity = 1;
+    guestNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
   });
 
-let guestNumberDelete1 = document.querySelector('.qnd1');
 
 guestNumberDelete1.addEventListener('click', function() {
   if(guestNumberAmount1.textContent == 0) {
     guestNumberAmount1.textContent = 0;
+    guestNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
   }
   else {
     guestNumberAmount1.textContent = parseInt(guestNumberAmount1.textContent) - 1;
@@ -36,15 +41,18 @@ guestNumberDelete1.addEventListener('click', function() {
 
 let guestNumberAdd2 = document.querySelector('.qna2');
 let guestNumberAmount2 = document.querySelector('.qnam2');
+let guestNumberDelete2 = document.querySelector('.qnd2');
 
 guestNumberAdd2.addEventListener('click', function() {
     guestNumberAmount2.textContent = parseInt(guestNumberAmount2.textContent) + 1;
+    dropdownBoxClear.style.opacity = 1;
+    guestNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
   });
 
-let guestNumberDelete2 = document.querySelector('.qnd2');
 guestNumberDelete2.addEventListener('click', function() {
     if(guestNumberAmount2.textContent == 0) {
       guestNumberAmount2.textContent = 0;
+      guestNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
     }
     else {
       guestNumberAmount2.textContent = guestNumberAmount2.textContent - 1;
@@ -54,32 +62,44 @@ guestNumberDelete2.addEventListener('click', function() {
 
 let guestNumberAdd3 = document.querySelector('.qna3');
 let guestNumberAmount3 = document.querySelector('.qnam3');
+let guestNumberDelete3 = document.querySelector('.qnd3');
 
 guestNumberAdd3.addEventListener('click', function() {
     guestNumberAmount3.textContent = parseInt(guestNumberAmount3.textContent) + 1;
+    dropdownBoxClear.style.opacity = 1;
+    guestNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.5);border: 1px solid rgba(31, 32, 65, 0.5)`;
 });
 
-let guestNumberDelete3 = document.querySelector('.qnd3');
 
 guestNumberDelete3.addEventListener('click', function() {
   if(guestNumberAmount3.textContent == 0) {
     guestNumberAmount3.textContent = 0;
+    guestNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
   }
   else {
     guestNumberAmount3.textContent = guestNumberAmount3.textContent - 1;
   }
 });
 
+// guestNumberAmount.forEach(function(item) {
+//   if(item.textContent == 0) {
+//     dropdownBoxClear.style.opacity = 0;
+//   }
+// });
 
 
-let dropdownBoxClear = document.querySelector('.dropdown-box__clear');
+
 // dropdownBoxButtonClear.forEach(function (entry) {
   dropdownBoxClear.addEventListener('click', function() {
     // event.preventDefault();
     guestNumberAmount1.textContent = 0;
-    guestNumberAmount.forEach(function() {
-      guestNumberAmount.textContent = '0';
-    });
+    guestNumberAmount2.textContent = 0;
+    guestNumberAmount3.textContent = 0;
+    dropdownBoxClear.style.opacity = 0;
+    guestNumberDelete1.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+    guestNumberDelete2.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+    guestNumberDelete3.style.cssText = `color:rgba(31, 32, 65, 0.25);border: 1px solid rgba(31, 32, 65, 0.25)`;
+    // guestNumberAmount.textContent = 0;
 
   });
 // });
