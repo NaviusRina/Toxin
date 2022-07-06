aa46f7383126cf7e93d4d11ba2e92a9c4206c6d0
Navis Rina <karya.09@mail.ru>
2022-07-06 23:10:23 +0300

    collected all js
    set up the assembly of all js files in main

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,7 @@ import $ from "jquery";
 (function (requireContext) {
     return requireContext.keys().map(requireContext);
 } (require.context('../', true, /(\.scss$)|(\.css$)/)));
+
+(function (requireContext) {
+    return requireContext.keys().map(requireContext);
+} (require.context('../', true, /(\.js)/)));
