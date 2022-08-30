3b4ce5dd1d26f9e79eaaaf58d2de6309fe15159b
Navis Rina <karya.09@mail.ru>
2022-08-30 23:01:22 +0300

    made registration page styles
    made registration page styles, review of some block styles

--- src/pug/pages/Registration/registration.js
+++ src/pug/pages/Registration/registration.js
@@ -1,1 +1,13 @@
-// import './registration.scss'
+let registrationSignInButton = document.querySelector(".account-registration__sign-in__button");
+let registrationCreateButton = document.querySelector(".sign-in-account__button");
+let registrationForm = document.querySelector(".registration__form");
+let registrationSignIn = document.querySelector(".registration__sign-in");
+
+registrationSignInButton.addEventListener('click', function() {
+    registrationForm.style.display = "none";
+    registrationSignIn.style.display = "inline-block";
+});
+registrationCreateButton.addEventListener('click', function() {
+    registrationForm.style.display = "inline-block";
+    registrationSignIn.style.display = "none";
+});
